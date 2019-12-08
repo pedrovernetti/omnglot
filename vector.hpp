7970c83366ffe92692a82f7497f306b849bfb4a9
pedrovernetti <pedrovernetti@gmail.com>
2019-12-08 19:25:05 -0300

    added a '(c)' to copyright notices.. just that

--- vector.hpp
+++ vector.hpp
@@ -1,6 +1,6 @@
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * Copyright 2019 Pedro Vernetti G.
+ * Copyright (c) 2019 Pedro Vernetti G.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this
  * software and associated documentation files (the "Software"), to deal in the Software
